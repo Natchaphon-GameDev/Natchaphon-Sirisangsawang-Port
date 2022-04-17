13189ae94fcc9c943db9dd087b1f8494ad641a73
Natchaphon-GameDev <nsp.gamedev@gmail.com>
2022-04-18 03:03:18 +0700

    Add to Deploy

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/Natchaphon-GameDev/Natchaphon-Sirisangsawang.github.io.git";
+const repoURL = "https://github.com/Natchaphon-GameDev/Portfolio.git";
 
 ghpages.publish(
   pathname,
